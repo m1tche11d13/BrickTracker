502a524d7398499db1414e763f083f6aea42f7e1
Daniel Mitchell <dm574@mail.umkc.edu>
2017-12-13 16:38:16 -0600

    Added the activity and layout for the activity to get sets

---
+++ app/src/main/java/com/mitchell/daniel/brickinventory/GetSetActivity.java
@@ -0,0 +1,13 @@
+package com.mitchell.daniel.brickinventory;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GetSetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_set);
+    }
+}
